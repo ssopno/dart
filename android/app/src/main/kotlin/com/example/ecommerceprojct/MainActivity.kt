bf97832dbecdaaeac3b153736f75c624940a1de9
Shathi Islam <116778673+ssopno@users.noreply.github.com>
2023-04-28 10:09:12 +0600

    ecommerce project

---
+++ android/app/src/main/kotlin/com/example/ecommerceprojct/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecommerceprojct
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const CraftyBay());
+}
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+     home: ,
+    );
+  }
+}
